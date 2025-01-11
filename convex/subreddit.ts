6f7b0a54e6497227e1ead8f89beb1980c2dd2a7b
David Park <davidpark.kh@gmail.com>
2025-01-11 16:56:37 -0700

    changed the convex subreddit.ts to just return null instead of error when no subreddit is found, and also finished the subreddit page.

--- convex/subreddit.ts
+++ convex/subreddit.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values"
-import { mutation } from "./_generated/server"
+import { mutation, query } from "./_generated/server"
 import { getCurrentUserOrThrow } from "./users"
 
 // this is how we will create a new subreddit
@@ -20,5 +20,26 @@ export const create = mutation({
 			description: args.description,
 			authorId: user._id,
 		})
+	},
+})
+
+// this is how we will query for a subreddit
+export const get = query({
+	args: { name: v.string() },
+	handler: async (ctx, args) => {
+		const subreddit = await ctx.db
+			.query("subreddit")
+			.filter((q) => q.eq(q.field("name"), args.name))
+			.unique()
+
+		// if (!subreddit) {
+		// 	throw new ConvexError({ message: "Subreddit not found." })
+		// }
+
+		// Instead of throwing an error like above, we can return null if the subreddit is not found, so that we can just return the message "subreddit not found" in the UI, rather having to handle the error thrown.
+		// This way, things can be simpler.
+		if (!subreddit) return null
+
+		return subreddit
 	},
 })
