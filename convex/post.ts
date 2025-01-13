4709d8fb3431e92670496983e5d5bc74a352ae33
David Park <davidpark.kh@gmail.com>
2025-01-12 17:35:49 -0700

    Started to adjust the post functionality with enriched post on post.ts

--- convex/post.ts
+++ convex/post.ts
@@ -1,6 +1,26 @@
-import { mutation } from "./_generated/server"
+import { mutation, QueryCtx } from "./_generated/server"
 import { ConvexError, v } from "convex/values"
 import { getCurrentUserOrThrow } from "./users"
+import { Doc, Id } from "./_generated/dataModel"
+
+// We want to include the name or information about the author and/or the subreddit, not just the ID.
+// Right now, we only have the ID of the author and the subreddit when we look at the post table defined in the schema.
+type EnrichedPost = Omit<Doc<"post">, "subreddit"> & {
+	author: { username: string } | undefined
+	subreddit:
+		| {
+				_id: Id<"subreddit">
+				name: string
+		  }
+		| undefined
+	imageUrl?: string
+}
+
+const ERROR_MESSAGES = {
+	POST_NOT_FOUND: "Post not found",
+	SUBREDDIT_NOT_FOUND: "Subreddit not found",
+	UNAUTHORIZED_DELETE: "You are not authorized to delete this post",
+} as const
 
 export const create = mutation({
 	args: {
@@ -21,3 +41,18 @@ export const create = mutation({
 		return postId
 	},
 })
+
+async function getEnrichedPost(ctx: QueryCtx, post: Doc<"post">): Promise<EnrichedPost> {
+	const [author, subreddit] = await Promise.all([ctx.db.get(post.authorId), ctx.db.get(post.subreddit)])
+	const image = post.image && (await ctx.storage.getUrl(post.image))
+
+	return {
+		...post,
+		author: author ? { username: author.username } : undefined,
+		subreddit: {
+			_id: subreddit!._id,
+			name: subreddit!.name,
+		},
+		imageUrl: image ?? undefined,
+	}
+}
